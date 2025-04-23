cce2af218e86b510c3b0412ea3c4da6095051e89
TestgroupJira <testgroup.jira@testgroup.cl>
2025-04-23 12:58:03 -0400

    actualizacion

--- src/main/java/demo/demo_google.java
+++ src/main/java/demo/demo_google.java
@@ -20,7 +20,9 @@ public class demo_google {
 
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://google.com");
-        System.out.println("Título de la página: " + driver.getTitle());
+        titulo = driver.getTitle();
+        System.out.println("Título de la página: " + titulo);
+        Assert.assertEquals("Google", titulo)
         Thread.sleep(2000);
         driver.quit();
     }
